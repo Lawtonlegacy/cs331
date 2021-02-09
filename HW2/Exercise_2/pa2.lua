bba40b984db08a61ce3394474a4581e4ecb63793
Lawtonlegacy <rjlawton@alaska.edu>
2021-02-09 00:01:16 -0800

    Passed all tests. Added comments.

--- HW2/Exercise_2/pa2.lua
+++ HW2/Exercise_2/pa2.lua
@@ -3,10 +3,15 @@
 -- 03 February 2021
 -- pa2 module for CS 331: HW2 Exercise 2 
 
+-- create module
 local pa2 = {}
 
 -- function filterArray
-
+-- Returns an array containing every value v in t for
+-- which p(v) is truthy, in the same order as they appear in t.
+-- Preconditions:
+--      "p" must be a single-parameter function that accepts any value in array "t"
+--      "t" must be a an array like table
 function pa2.filterArray(p,t)
     local arr = {}
     local i = 1
@@ -19,10 +24,20 @@ function pa2.filterArray(p,t)
     return arr
 end
 
+-- function concatMax
+-- Takes a string and an integer. Returns a string which is a which is the concatenation of as
+-- many copies of the given string as possible, without the length exceeding the given integer.
+-- Preconditions:
+--      "str" is a valid string object.
+--      "int" is a valid number object with no decimal.
 function pa2.concatMax(str, int)
     return string.rep(str,math.floor(int/str:len()))
 end
 
+-- function collatz
+-- collatz takes an integer parameter k and returns an iterator that produces one or more integers.
+-- Preconditions:
+--      "k" is a valid positive number object with no decimal.
 function pa2.collatz(k)
     local function iterator()
         if k == 1 then
@@ -41,20 +56,29 @@ function pa2.collatz(k)
     return iterator
 end
 
+-- function substrings
+-- Takes a single parameter s, which must be a string. It yields all substrings of s: 
+-- first the unique length-zero substring, then all length-one substrings, and so on, ending with s itself.
+-- Uses coroutine module with function.
+-- Preconditions:
+--      "s" is a vaild string object
 function pa2.substrings(s)
     -- base case empty string
     coroutine.yield("")
     local len = s:len()
-    local sublen = 1
-    local from = 1
+    local sublen = 0
+    local from = 0
     local to = 0
-    local numsubs = len*(len+1)/2
+    local numsubs = (len*(len+1)/2)+len
 
     for i=1, numsubs do
-        to = to+1
+        from = from+1
+        to = from + sublen
+        -- increment sublen
         if to > len then
-            from = from+1
-            to = from
+            sublen = sublen + 1
+            from = 0
+            to = 0
         end
         local substring = s:sub(from,to)
         if #substring ~= 0 then
@@ -64,7 +88,5 @@ function pa2.substrings(s)
     end
 end
 
-
-
-
+-- export module
 return pa2
